3f348fe061053dfd075ba2bff8ba10ff801a41ae
Matjaz Ciglic <matjaz.ciglic.jst@gmail.com>
2020-05-03 00:23:59 +0200

    Set up player rotation

---
+++ VizualizacijaProstora/Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    public float rotSpeed;
+
+    float xRotation = 0f;
+    float yRotation = 0f;
+
+    private void Start() {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void LateUpdate() {
+        Rotate();
+    }
+
+    void Rotate() {
+        // get mouse input
+        float x = Input.GetAxis("Mouse X") * rotSpeed * Time.deltaTime;
+        float y = Input.GetAxis("Mouse Y") * rotSpeed * Time.deltaTime;
+
+        // calculate rotation
+        yRotation += x;
+        xRotation -= y;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        // apply rotation
+        transform.localRotation = Quaternion.Euler(xRotation, yRotation, 0f);
+    }
+}
