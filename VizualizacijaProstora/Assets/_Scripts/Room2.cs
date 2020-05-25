7d49bf875e87c180ae174258bf8175300bbc0674
Matjaz Ciglic <matjaz.ciglic.jst@gmail.com>
2020-05-25 19:27:29 +0200

    Added instructions

--- VizualizacijaProstora/Assets/_Scripts/Room2.cs
+++ VizualizacijaProstora/Assets/_Scripts/Room2.cs
@@ -3,5 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Room2 : MonoBehaviour {
+
+    /// <summary>
+    /// How to use this:
+    /// 1. Each room must have the "Room" Tag and this script attached
+    /// 2. The starting room must be set to active and in the Main Camera Player Script, it should be added as currentRoom
+    /// 3. Each room this one connects to must be listed among the neighbors.
+    /// 4. Null values among neighbors are handled
+    /// </summary>
     public GameObject[] Neighbors;
 }
