802c62f59b62ec4d0415d9dc8d47d976833b5cfd
Matjaz Ciglic <matjaz.ciglic.jst@gmail.com>
2020-05-25 19:43:00 +0200

    Add instructions, extend test map for a slightly bizzare case

--- VizualizacijaProstora/Assets/_Scripts/Room2.cs
+++ VizualizacijaProstora/Assets/_Scripts/Room2.cs
@@ -8,8 +8,9 @@ public class Room2 : MonoBehaviour {
     /// How to use this:
     /// 1. Each room must have the "Room" Tag and this script attached
     /// 2. The starting room must be set to active and in the Main Camera Player Script, it should be added as currentRoom
-    /// 3. Each room this one connects to must be listed among the neighbors.
-    /// 4. Null values among neighbors are handled
+    /// 3. The room must also have a trigger collider
+    /// 4. Each room this one connects to must be listed among the neighbors.
+    /// 5. Null values among neighbors are handled
     /// </summary>
     public GameObject[] Neighbors;
 }
