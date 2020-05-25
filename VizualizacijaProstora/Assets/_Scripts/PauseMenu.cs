12e8ffd2ca60d022d6b3871d5bd05da04dff6a40
Matjaz Ciglic <matjaz.ciglic.jst@gmail.com>
2020-05-25 20:59:17 +0200

    Implemented pause button

---
+++ VizualizacijaProstora/Assets/_Scripts/PauseMenu.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+
+    public static bool paused = false;
+    public GameObject ui;
+
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.P)) {
+            TogglePause();
+        }
+    }
+
+    public void TogglePause() {
+        paused = !paused;
+        ui.SetActive(paused);
+
+        if (paused) {
+            // pause
+            Time.timeScale = 0f;
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        } else {
+            // resume
+            Time.timeScale = 1f;
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+    }
+
+    public void ExitGame() {
+
+    }
+}
